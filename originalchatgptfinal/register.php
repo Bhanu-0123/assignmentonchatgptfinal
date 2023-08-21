3a4031f1d28577c87c66a58cd6d92725c4abbcc7
Bhanu-0123 <137532161+Bhanu-0123@users.noreply.github.com>
2023-08-21 08:23:40 +0530

    Add files via upload

---
+++ originalchatgptfinal/register.php
@@ -0,0 +1,29 @@
+<?php
+// Database connection
+$servername = "your-server-name";
+$username = "your-username";
+$password = "your-password";
+$dbname = "your-database-name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Form data handling
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO registrations (name, email) VALUES ('$name', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
